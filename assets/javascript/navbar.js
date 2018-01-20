544de0a9be3f4a4f9074ef39b20185df2c72da58
Amanda <amandapbui@berkeley.edu>
2018-01-19 20:53:55 -0800

    Clickable navbar

---
+++ assets/javascript/navbar.js
@@ -0,0 +1,15 @@
+/**
+ * Created by amandabui on 1/17/18.
+ */
+//function myFunction(x) {
+//    x.classList.toggle("change");
+//}
+
+function openNav() {
+    document.getElementById("navcontainer").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("navcontainer").style.display = "none";
+}
+
